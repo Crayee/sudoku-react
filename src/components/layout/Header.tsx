ca969f59e3bc598ad207ce3a9fb3f2f15ceb205e
Till Gohl <till.crapp@gmail.com>
2023-01-11 20:35:49 +0100

    Version 1 beta

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,7 +10,7 @@ type HeaderProps = {
 export const Header = (props: HeaderProps) => {
     return (
         <header className="header">
-            <h1>Shawnas Sudoku</h1>
+            <h1>Shawnas Sudoku V1</h1>
             <h2 onClick={props.onClick}>New Game</h2>
         </header>
     )
